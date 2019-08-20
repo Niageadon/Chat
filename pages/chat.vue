ac59add2b195adb9eb6af1966de5292dc416d266
Niageadon <legoog.tsop@gmail.com>
2019-08-20 08:13:13 +0300

    ref chat page

--- pages/chat.vue
+++ pages/chat.vue
@@ -13,10 +13,16 @@
             ></v-textarea></v-flex>
             <v-btn @click="send">send</v-btn>
             <!--chat window-->
-            <v-flex xs6><ul>
-                <li v-for="(message,id) in messages" :key="id">{{message.text}}</li>
-            </ul></v-flex>
-
+            <v-flex xs10 class=""  style="max-height: 300px; background-color: #424242">
+                <v-list two-line class="ma-4">
+                    <template v-for="(message,id) in messages">{{message.autor.name, message.text}}">
+                        <v-list-tile-content :key="id">
+                            <!--<v-list-tile-title v-html="message.autor"></v-list-tile-title>
+                            <v-list-tile-sub-title v-html="message.userText"></v-list-tile-sub-title>-->
+                        </v-list-tile-content>
+                    </template>
+                </v-list>
+            </v-flex>
         </v-layout>
     </v-container>
 </template>
